7a347d14540528816246ba3821df74ce420a55ff
John McCormick <jam@neardark.com>
2014-06-07 16:22:00 -0500

    Theme homepage feature

--- profiles/dcco/themes/dcco2014/js/main.js
+++ profiles/dcco/themes/dcco2014/js/main.js
@@ -1,5 +1,28 @@
 (function ($) {
   $(document).ready(function(){
 
+      stylePromoImage();
+
+      // Activate Slick carousel on landing page promo.
+      $('.promos__wrapper').slick({
+          slide: 'li',
+          arrows: false,
+          autoplay: true,
+          autoplaySpeed: 5000
+      });
+
+      /**
+       * Add an inline style tag for the background promo image, based off of its
+       * data-promo-background attribute because views disallows style tags on field
+       * rewrites for security purposes.
+       */
+      function stylePromoImage() {
+          var $promos = $('.promo__image a');
+          $.each($promos, function () {
+              var promoImage = $(this)[0].attributes;
+              var promoBackground = $(this)[0].attributes[0].nodeValue;
+              $(this).attr('style', 'background-image: url(' + promoBackground + ')');
+          });
+      }
   });
 })(jQuery);
