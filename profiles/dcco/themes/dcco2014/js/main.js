c387ed96eabd36dfcd4c1315e99d8bef675df08e
Dano Manion <danomanion@gmail.com>
2014-06-12 17:48:08 -0600

    Issue #2284545 by Dano Manion: remove commented code.

--- profiles/dcco/themes/dcco2014/js/main.js
+++ profiles/dcco/themes/dcco2014/js/main.js
@@ -8,11 +8,6 @@
       $mainMenu.find('.sponsors a').addClass('icon-heart');
       $mainMenu.find('.travel a').addClass('icon-plane');
 
-//    var maxHeight = 0;
-//    jQuery('div.views-field-field-sponsor-logo').each(function() {
-//      maxHeight = Math.max(maxHeight, jQuery(this).height());
-//    }).height(maxHeight);
-
       // Activate Slick carousel on landing page promo.
       $('.promos__wrapper').slick({
           slide: 'li',
@@ -22,10 +17,9 @@
       });
   });
 
-  // Wait for images to load.
+  // Fire events after images load.
   $(window).load(function() {
-
-    // Set equal heights to sponsor images so that we can vertically center them.
+    // Set equal heights to sponsor image containers so that we can vertically center them in the stylesheet.
     var maxHeight = 0;
     $('.views-field-field-sponsor-logo').each(function() {
       maxHeight = Math.max(maxHeight, $(this).height());
